a5e25333f632ee5a2abe718f6527bd5f7185dc33
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-18 19:05:46 +0330

    webpack configuration changed entry file output path uses common/config.assets.path

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
+var config = require('./src/common/config').default
 var webpack = require('webpack')
 
 var PROD = (process.env.PROD_ENV || 'dev') === 'prod'
 
 module.exports = {
-  entry: './src/app.web.js',
+  entry: './src/browser/index.js',
   output: {
-    path: __dirname + '/src/server/public',
+    path: config.assets.path,
     filename: PROD ? 'bundle.min.js' : 'bundle.js'
   },
   module: {
