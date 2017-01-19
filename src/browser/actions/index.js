54683f6bd7d9911d9fa395d4f33d7d68e3320538
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-20 01:27:24 +0330

    cleaned action types

--- src/browser/actions/index.js
+++ src/browser/actions/index.js
@@ -1,3 +1,16 @@
+/**
+ * UX action types
+ */
+
+ // redirect url action types
 export const SET_REDIRECT_URL = 'SET_REDIRECT_URL'
+export const NAVIGATE_TO_REDIRECT_URL = 'NAVIGATE_TO_REDIRECT_URL'
+
+/**
+ * API action types
+ */
+
+// authentication
 export const LOGIN = 'LOGIN'
-export const NAVIGATE_TO_SPECIFIED_URL = 'NAVIGATE_TO_SPECIFIED_URL'
+export const LOGOUT = 'LOGOUT'
+
