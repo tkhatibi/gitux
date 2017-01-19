d4cb24c5299124388cb0361dc0845e011e3c19b6
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-19 18:58:35 +0330

    added routing: routerReducer from react-router-redux to reducers

--- src/browser/reducers.js
+++ src/browser/reducers.js
@@ -2,10 +2,11 @@
  * Root Reducer
  */
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 
 // Import Reducers
 
 // Combine all reducers into one root reducer
 export default combineReducers({
-  
+  routing: routerReducer,
 })
