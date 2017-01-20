b6a57212a9b5ec7d12a2fe58b81cdcb2b3e5565f
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-20 15:33:10 +0330

    added getServerToken promise to server githubApi config

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,8 @@
 import path from 'path'
 import config from '../common/config'
+import axios from 'axios'
 
-const server = {
+const serverConfig = {
   ...config,
   view: {
     engine: 'ejs',
@@ -9,9 +10,16 @@ const server = {
   },
   gihubApi: {
     clientSecret: 'feec23a7f779b67cd89a631245ec3f669508b26d',
-    // clientId: 'c2984d0f08b17be909b1',
-    authCallbackUrl: '/auth-cb?code=:currentUserCode',
+    authCallbackUrl: '/auth-cb?code=:currentUserCode', // It is used in express
+    getAccessTokenUrl: currentUserCode => {
+      return `https://github.com/login/oauth/access_token?client_id=${config.gihubApi.clientId}&client_secret=${this.clientSecret}&code=${currentUserCode}`
+    },
+    getAccessToken: currentUserCode => {
+      return axios.post(this.getAccessToken(currentUserCode))
+        .then(res => console.log(res))
+        .catch(error => console.error(error))
+    }
   },
 }
 
-export default server
+export default serverConfig
