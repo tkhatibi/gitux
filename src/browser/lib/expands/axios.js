acc79220890383b7809f0d4c3bc2d48af55651cb
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-21 01:08:26 +0330

    added Authorization header to custom axios

--- src/browser/lib/expands/axios.js
+++ src/browser/lib/expands/axios.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import { getAccessToken } from '../helpers/auth-helper'
 
 export default axios.create({
-  baseURL: '/api/',
-  timeout: 1000,
+  // baseURL: '/api/',
+  timeout: 3000,
   headers: {
-    // Authorization: `token ${oauthToken}`
+    Authorization: `token ${getAccessToken()}`,
     'X-Requested-With': 'XMLHttpRequest',
     // 'X-Custom-Header': 'foobar',
   }
