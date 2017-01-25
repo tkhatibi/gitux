7c97c8d6d0249bd95795d64d0ced3504c9558812
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-25 14:13:25 +0330

    updated Repository card component

--- src/browser/components/Repository/Repository.js
+++ src/browser/components/Repository/Repository.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 // Material UI
 import {
@@ -7,23 +8,35 @@ import {
   CardHeader,
   CardText,
 } from 'material-ui/Card'
-import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
+import Chip from 'material-ui/Chip'
 
 const Repository = props => (
   <Card>
     <CardHeader
-      title={props.name}
-      subtitle={props.owner.login}
+      title={props.full_name}
+      subtitle={props.private ? 'Private' : 'Public'}
       avatar={props.owner.avatar_url}
     />
     <CardText>
       {props.description}
+      <p><b>Language : </b>{props.language}</p>
+      <p><b>Last Update : </b>{props.updated_at}</p>
     </CardText>
     <CardActions>
-      <FlatButton label="Action1" />
-      <FlatButton label="Action2" />
+      <RaisedButton label={`Star (${props.stargazers_count})`} disabled={!props.isLoggedIn} />
+      <RaisedButton label={`Fork (${props.forks_count})`} disabled={!props.isLoggedIn || !!props.fork} />
+      <RaisedButton label={`Issue (${props.open_issues_count})`} disabled={!props.isLoggedIn} />
+      <RaisedButton label={`Watch (${props.watchers_count})`} disabled={!props.isLoggedIn} />
     </CardActions>
   </Card>
 )
 
-export default Repository
+const mapStateToProps = (state, ownProps) => ({
+  isLoggedIn: state.auth.isLoggedIn,
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Repository)
