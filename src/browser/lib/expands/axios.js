40fb2e45106c411ada5d53d67d8d6636d9278438
Tooraj Khatibi <2rajpx@gmail.com>
2017-01-25 13:28:55 +0330

    added conditional headers in axios configuration

--- src/browser/lib/expands/axios.js
+++ src/browser/lib/expands/axios.js
@@ -1,12 +1,21 @@
 import axios from 'axios'
 import { getAccessToken } from '../helpers/auth-helper'
 
+const configureHeaders = () => {
+  let headers = {}
+  const accessToken = getAccessToken()
+  if (typeof accessToken !== 'undefined') {
+    headers.Authorization = `token ${accessToken}`
+  }
+  return headers
+}
+
 export default () => axios.create({
   baseURL: 'https://api.github.com/',
   // timeout: 10000,
   headers: {
-    Authorization: `token ${getAccessToken()}`,
     'X-Requested-With': 'XMLHttpRequest',
+    ...configureHeaders(),
     // 'X-Custom-Header': 'foobar',
   }
 })
